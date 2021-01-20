0f59db1ad10574f6754475a93d75f45a6f7737dd
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-20 09:51:48 +0100

    Exercice 6

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercice_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(week.Lundi);
+            Console.WriteLine(week.Mardi);
+            Console.WriteLine(week.Mercredi);
+            Console.WriteLine(week.Jeudi);
+            Console.WriteLine(week.Vendredi);
+            Console.WriteLine(week.Samedi);
+            Console.WriteLine(week.Dimanche);
+            Console.WriteLine("\r\n" + week.Jeudi);
+        }
+        public enum week
+        {
+            Lundi,
+            Mardi,
+            Mercredi,
+            Jeudi,
+            Vendredi,
+            Samedi,
+            Dimanche
+        }
+    }
+}
